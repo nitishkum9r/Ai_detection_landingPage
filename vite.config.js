82b5bef057961f4ea8bb9bad18ef003ee92fccc5
nitishkum9r <nitish.factor@gmail.com>
2025-05-20 12:45:19 +0530

    website live commit2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Ai_detection_landingPage/',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js', // Explicitly set postcss config path
